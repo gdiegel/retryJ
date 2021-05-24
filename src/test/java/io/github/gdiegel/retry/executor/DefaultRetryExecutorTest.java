94e6a4d22a5e791d4b764e0e648823d787fa97f6
Gabriel Diegel <gabriel.diegel@icloud.com>
2021-05-23 19:25:11 -0500

    Add test

--- src/test/java/io/github/gdiegel/retry/executor/DefaultRetryExecutorTest.java
+++ src/test/java/io/github/gdiegel/retry/executor/DefaultRetryExecutorTest.java
@@ -1,20 +1,34 @@
 package io.github.gdiegel.retry.executor;
 
 import io.github.gdiegel.retry.BaseTest;
+import io.github.gdiegel.retry.collaborators.InvocationCounter;
 import io.github.gdiegel.retry.policy.RetryPolicy;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.Duration;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultRetryExecutorTest extends BaseTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 100000})
+    @ValueSource(ints = {0, 1, 2})
     void shouldExecuteZeroTimesIfMaximumExecutionsIsZero(int maximumExecutions) {
         final var retryPolicy = RetryPolicy.<Integer>builder().withMaximumExecutions(maximumExecutions).build();
         final var retryExecutor = new DefaultRetryExecutor<>(retryPolicy);
         retryExecutor.execute(IDEMPOTENT_CALLABLE);
         assertThat(retryExecutor.getCurrentExecutions()).isEqualTo(maximumExecutions);
+    }
+
+    @Test
+    void shouldHoldLastComputedValueWhenExhaustionIsReached() {
+        final var invocationCounter = new InvocationCounter();
+        final var retryPolicy = RetryPolicy.<Long>builder().withTimeout(Duration.ofSeconds(5)).throwing(false).build();
+        final var retryExecutor = new DefaultRetryExecutor<>(retryPolicy);
+        final Optional<Long> result = retryExecutor.execute(invocationCounter::invoke);
+        assertThat(result).hasValueSatisfying(invocations -> assertThat(invocations).isPositive());
     }
 }
